0f18cb19c7ae9f597a71ef3292a56c47478df4ea
MarchenkoVladislav <vlad25091999@mail.ru>
2020-09-12 16:39:39 +0300

    Update common module

--- common/src/main/kotlin/com/companion/common/model/dao/AbstractDAO.kt
+++ common/src/main/kotlin/com/companion/common/model/dao/AbstractDAO.kt
@@ -10,46 +10,27 @@ import kotlin.streams.toList
 /**
  * @author Vladislav Marchenko
  */
-@Repository
+@Suppress("UNCHECKED_CAST")
 abstract class AbstractDAO<T: Serializable, ID>  {
 
-    private val clazz: String = this::class.simpleName!!
+    protected val clazz: Class<T> = (this::class.java.genericSuperclass as java.lang.reflect.ParameterizedType).actualTypeArguments[0] as Class<T>
 
     @Autowired
     lateinit var sessionFactory: SessionFactory
 
-    @Suppress("UNCHECKED_CAST")
     fun findById(id: ID) : T? = sessionFactory.currentSession
-            ?.createQuery("From " + clazz + " where " + convertClassNameToIdName(clazz) + " = :id")
+            ?.createQuery("From ${clazz.name} where id = :id")
+            ?.setParameter("id", id)
             ?.list()
             ?.get(0) as T?
 
-    @Suppress("UNCHECKED_CAST")
     fun findAll() : List<T> = sessionFactory
-            .currentSession?.createQuery("From $clazz")
+            .currentSession?.createQuery("From ${clazz.name}")
             ?.list() as List<T>
 
     open fun delete(obj: T) : Unit? = sessionFactory.currentSession?.delete(obj)
 
-    @Suppress("UNCHECKED_CAST")
-    open fun save(obj: T) : T? = sessionFactory.currentSession?.save(obj) as T?
-
-    open fun update(obj: T) : Unit? = sessionFactory.currentSession?.update(obj)
-    
-    private fun convertClassNameToIdName(className : String) : String {
-        var res = className.replace("Entity", "_id")
-
-        var myMap = mutableMapOf<String, String>()
-
-        res.chars()
-                .filter { it.toChar().isUpperCase() }
-                .toList()
-                .map{myMap[it.toChar().toString()] = "_" + it.toChar().toString().toLowerCase()}
-
-        myMap.forEach{ (t, u) -> res = res.replace(t, u) }
-
-        return res.removePrefix("_")
-    }
+    open fun saveOrUpdate(obj: T) : T? = sessionFactory.currentSession?.saveOrUpdate(obj) as T?
 
 }
 
